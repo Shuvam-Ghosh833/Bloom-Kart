0d7dbdaa4fa8646f1f843c3d4fbf6d9e00111f5a
Shuvam Ghosh <shuvamghosh1234@gmail.com>
2024-01-06 18:35:59 +0530

    working on routes api 2

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,7 +1,10 @@
+//All the callback Functions required for http method are stored here
+
+//importing Product Models Schema
 const Product=require("../models/productModels");
 
 
-//create products(ADMIN )
+//create products (ADMIN)
 exports.createProduct= async(req, res, next) => {
     const product=await Product.create(req.body)
     res.status(201).json({success:true,product})
@@ -11,4 +14,45 @@ exports.createProduct= async(req, res, next) => {
 exports.getAllProduct = async(req, res, next) => {
     const product=await Product.find();
     res.status(200).json({success:true,product})
+}
+
+//Get Product Details
+exports.getProductDetails=async(req,res,next) => {
+    const product=await Product.findById(req.params.id);      
+    if(!product)
+    {
+        return res.status(500).json({ success: false , message: "Product not found"});
+    }
+    
+    res.status(200).json({success:true,product})
+
+}
+
+//Update Product (ADMIN)
+exports.updateProduct=async(req,res,next) => {
+    let product=await Product.findById(req.params.id);       //taking 'let' so we can change the product
+    if(!product)
+    {
+        return res.status(500).json({ success: false , message: "Product not found"});
+    }
+    product=await Product.findByIdAndUpdate(req.params.id,req.body,{
+        new:true,
+        runValidators:true,
+        useFindAndModify:false
+    }); 
+    
+    res.status(200).json({success:true,product})
+
+}
+
+//Delete Product (ADMIN)
+exports.deleteProduct= async(req, res, next) => {
+    const product=await Product.findById(req.params.id);  
+    if(!product)
+    {
+        return res.status(500).json({ success: false , message: "Product not found"});
+    }
+    await product.deleteOne();
+    res.status(201).json({success:true, message: "Product deleted Successfully"});
+
 }
