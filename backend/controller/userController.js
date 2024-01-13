bc395883a007f94d53d2bbafeb361881559994db
Shuvam Ghosh <shuvamghosh1234@gmail.com>
2024-01-13 18:26:02 +0530

    User Routes:-  Update User Profile

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -168,3 +168,21 @@ exports.updatePassword=catchAsyncErrors(async(req,res,next) => {
 });
 
 
+//Update User Profile
+exports.updateProfile=catchAsyncErrors(async(req,res,next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  // Add cloudinary later for avatar
+
+  const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({success: true});
+
+});
