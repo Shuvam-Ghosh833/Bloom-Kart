8ec8af0264523ae27daaf6fedb30fd402f8011bb
Shuvam Ghosh <shuvamghosh1234@gmail.com>
2024-01-14 21:35:26 +0530

    Order API - Create New Order

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -2,3 +2,31 @@ const Order=require("../models/orderModels");
 const Product=require("../models/productModels");
 const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+
+
+// Create new Order
+exports.newOrder = catchAsyncErrors(async (req, res, next) => {
+    const {
+      shippingInfo,
+      orderItems,
+      paymentInfo,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    } = req.body;
+  
+    const order = await Order.create({
+      shippingInfo,
+      orderItems,
+      paymentInfo,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+      paidAt: Date.now(),
+      user: req.user._id,
+    });
+  
+    res.status(201).json({success: true,order,});
+  });
