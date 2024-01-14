837bca21ae2ca6d385dd0f6c08d6507d1a4ff7b5
Shuvam Ghosh <shuvamghosh1234@gmail.com>
2024-01-14 21:49:14 +0530

    Order API - Get Single Order Details (ADMIN)

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -30,3 +30,17 @@ exports.newOrder = catchAsyncErrors(async (req, res, next) => {
   
     res.status(201).json({success: true,order,});
   });
+
+
+// Get Single Order Details
+exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
+    const order = await Order.findById(req.params.id).populate("user","name email");
+  
+    if (!order) {
+      return next(new ErrorHandler("Order not found with this Id", 404));
+    }
+  
+    res.status(200).json({success: true,order,});
+  });
+
+
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,9 +1,11 @@
 const express=require("express");
 const router=express.Router();
 const { isAuthenticatedUser ,authorizeRoles} = require("../middleware/auth");
-const { newOrder } = require("../controller/orderController");
+const { newOrder, getSingleOrder } = require("../controller/orderController");
 
 router.route("/order/new").post(isAuthenticatedUser ,newOrder);
+router.route("/order/:id").get(isAuthenticatedUser ,authorizeRoles("admin"),getSingleOrder);
+
 
 
 module.exports=router;
