50b785e85a6503e20db2854179431993e857152a
Shuvam Ghosh <shuvamghosh1234@gmail.com>
2024-01-18 23:07:26 +0530

    Frontend Home and Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 import WebFont from "webfontloader";
 import Header from "./component/layout/Header/Header.js"
 import Footer from "./component/layout/Footer/Footer.js"
+import Home from "./component/Home/Home.js"
 
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router,Route,Routes} from "react-router-dom";
 
 
 function App() {
@@ -21,6 +22,10 @@ function App() {
   return (
     <Router>
       <Header/>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      
+    </Routes>
       <Footer/>
     </Router>
     
